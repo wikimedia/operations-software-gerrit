b91b3bd2df05b3c19886ae30f41f5b515e1048f7
Antoine Musso <hashar@free.fr>
2024-06-04 15:17:21 +0200

    Use a wildcard TypeScript include for plugins
    The TypeScript config explicitly list each of the plugins files to
    include for type checking. That probably comes from before installing
    the @gerritcodereview/typescript-api module which defines the Gerrit
    interface.
    
    Using a wildcard `plugings/*.js` ensures future JavaScript plugins would
    be checked by TypeScript.
    
    Update `wm-app-theme` and `wm-custom_links` to use `window.Gerrit` which
    gets a type defined.
    
    Change-Id: Ia2f7c7b6573cd3a254290c354b609cd5b690df81

--- plugins/wm-app-theme.js
+++ plugins/wm-app-theme.js
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-Gerrit.install(plugin => {
+window.Gerrit.install(plugin => {
 
   const lightRules = `
     html.lightTheme {
